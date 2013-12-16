fa4327e903f99da106775dcfe5453316aeefaf05
aspirintr <kasimtasdemir@gmail.com>
2013-12-16 06:03:30 -0800

    emacs.el file is added

---
+++ emacs.el
@@ -0,0 +1,8 @@
+
+;;; Load color-theme
+(add-to-list 'load-path *my-color-theme-install-dir*)
+(require 'color-theme)
+(eval-after-load "color-theme"
+  '(progn
+     (color-theme-initialize)
+     (color-theme-hober)))
