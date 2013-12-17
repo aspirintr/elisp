4774d9b6bc608c3940142e83ab678fa34c2db71c
aspirintr <kasimtasdemir@gmail.com>
2013-12-17 01:08:22 -0800

    overwrite selection, show full path of the file, iswitchb mode t, are added to emacs.el

--- emacs.el
+++ emacs.el
@@ -6,3 +6,12 @@
   '(progn
      (color-theme-initialize)
      (color-theme-gnome2)))
+
+;;; Overwriting selected text
+(delete-selection-mode t)
+
+;;; Show The Full Path Of The Current File In The Frame Bar
+(setq frame-title-format '((:eval buffer-file-name)))
+
+;;; C-x b autocomplete buffer name
+(iswitchb-mode 1)
