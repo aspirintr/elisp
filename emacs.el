ecebde6afc6a57a1e80a06083c5d5345410e9811
aspirintr <kasimtasdemir@gmail.com>
2014-03-25 14:12:05 +0200

    code comment out M-; is added to emacs.el

--- emacs.el
+++ emacs.el
@@ -27,3 +27,17 @@
 (global-set-key (kbd "s-C-<right>") 'enlarge-window-horizontally)
 (global-set-key (kbd "s-C-<down>") 'shrink-window)
 (global-set-key (kbd "s-C-<up>") 'enlarge-window)
+
+;;;CODE EDITING
+;;;Comment line or add comment to the end
+(defun comment-dwim-line (&optional arg)
+  "Replacement for the comment-dwim command.
+        If no region is selected and current line is not blank and we are not at the end of the line,
+        then comment current line.
+        Replaces default behaviour of comment-dwim, when it inserts comment at the end of the line."
+  (interactive "*P")
+  (comment-normalize-vars)
+  (if (and (not (region-active-p)) (not (looking-at "[ \t]*$")))
+      (comment-or-uncomment-region (line-beginning-position) (line-end-position))
+    (comment-dwim arg)))
+(global-set-key "\M-;" 'comment-dwim-line)
